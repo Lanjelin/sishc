58935d78bfa4d57983aa45519f8439194a67b8ac
lanjelin <6988319+Lanjelin@users.noreply.github.com>
2025-01-15 12:19:27 +0100

    Added missing flag

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 
 if ! [[ "$PUID" == 0 && "$PGID" == 0 ]]; then
   # Change UID and GID based on environment variables
-  usermod -u "$PUID" abc >/dev/null
-  groupmod -g "$PGID" abc >/dev/null
+  usermod -o -u "$PUID" abc >/dev/null
+  groupmod -o -g "$PGID" abc >/dev/null
 
   find / -maxdepth 1 ! -name '.*' -exec chown abc:abc {} + >/dev/null 2>&1
 
